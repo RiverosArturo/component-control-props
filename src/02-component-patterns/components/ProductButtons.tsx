df3a131ffd5ee2d01f823e1c4cbf371b0f018f81
Arturo Riveros <riveros45rturo@gmail.com>
2024-10-02 18:31:25 -0600

    Fin del patron

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 
 import styles from '../styles/styles.module.css'
@@ -10,7 +10,15 @@ export interface Props {
 
 export const ProductButtons = ({ className, style }: Props) => {
 
-    const { increaseBy, counter } = useContext( ProductContext );
+    const { increaseBy, counter, maxCount } = useContext( ProductContext );
+
+    const isMaxReached = useCallback(
+      () => !!maxCount && counter === maxCount,
+      [counter, maxCount],
+    );
+    
+    //True si el count === maxCount
+    // False si no lo es
 
     return (
         <div 
@@ -24,7 +32,8 @@ export const ProductButtons = ({ className, style }: Props) => {
             <div className={ styles.countLabel }> { counter } </div>
 
             <button
-                className={ styles.buttonAdd }
+                disabled={ isMaxReached() }
+                className={ `${ styles.buttonAdd } ${ isMaxReached() && styles.disabled }` }
                 onClick={ () => increaseBy( +1 ) }> + </button>
         </div>
     );
--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,40 +1,61 @@
 import { useEffect, useRef, useState } from 'react'
-import { onChangeArgs, Product } from '../interfaces/interfaces';
+import { InitialValues, onChangeArgs, Product } from '../interfaces/interfaces';
 
 
 interface useProductArgs {
     product: Product;
-    onChange?: ( args: onChangeArgs ) => void;
+    onChange?: (args: onChangeArgs) => void;
     value?: number;
+    initialValues?: InitialValues;
 }
 
 
-export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
+export const useProduct = ({ onChange, product, value = 0, initialValues }: useProductArgs) => {
 
-    const [ counter, setCounter ] = useState( value );
+    const [counter, setCounter] = useState<number>(initialValues?.count || value);
+    //El componente no esta montado
+    const isMounted = useRef(false)
 
-    const isControlled = useRef( !!onChange );
+    // console.log(initialValues?.count);
+
+    const isControlled = useRef(!!onChange);
 
 
-    const increaseBy = ( value: number ) => {
+    const increaseBy = (value: number) => {
         //para obtener informacion de un hook ref usamos el .current
         // console.log('isControlled', isControlled.current );
-        if( isControlled.current ) return onChange!({ count: value, product })
-        
-      
-        const newValue = Math.max( counter + value, 0 )
-        setCounter( newValue );
+        if (isControlled.current) return onChange!({ count: value, product })
 
+        let newValue = Math.max(counter + value, 0)
+        if( initialValues?.maxCount ){
+            //Math.min toma el valor minimo de los dos que se pongan
+            newValue = Math.min( newValue, initialValues.maxCount )
+        }
+        setCounter(newValue);
         onChange && onChange({ count: newValue, product });
     }
 
+    const reset = () => {
+        setCounter( initialValues?.count|| value  )
+    }
+
     useEffect(() => {
-        setCounter( value );
-    }, [ value ])
+        if (!isMounted.current) return;
+        setCounter(value);
+    }, [value]);
+
+    useEffect(() => {
+        isMounted.current = true;
+    }, []);
+
 
     return {
         counter,
-        increaseBy
+        isMaxCountReached: !!initialValues?.count && initialValues.maxCount === counter,
+        maxCount: initialValues?.maxCount,
+
+        increaseBy,
+        reset,
     }
 
 }
